b0873c1e4be43d1c6829f9ba5770725128c0fcd4
Stephan Cedillo <stephan.cedillo@gmail.com>
2026-05-24 19:48:37 -0500

    Creacion Proyecto

---
+++ src/main/java/ups/edu/ec/bibleoteca/interfazbibleoteca/InterfazBibleoteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ups.edu.ec.bibleoteca.interfazbibleoteca;
+
+/**
+ *
+ * @author stephancedillo
+ */
+public class InterfazBibleoteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
