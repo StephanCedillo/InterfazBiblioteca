9686866a4c846d2683612113db914ca477e07e04
Stephan Cedillo <stephan.cedillo@gmail.com>
2026-05-25 03:15:15 -0500

    UPD: Cambios

--- src/main/java/ups/edu/ec/bibleoteca/interfazbibleoteca/InterfazBibleoteca.java
+++ src/main/java/ups/edu/ec/bibleoteca/interfazbibleoteca/InterfazBibleoteca.java
@@ -4,9 +4,6 @@
 
 package ups.edu.ec.bibleoteca.interfazbibleoteca;
 
-import java.awt.Frame;
-import ups.edu.ec.bibleoteca.interfazclases.Creacion.InterfazCreacion;
-
 /**
  *
  * @author stephancedillo
@@ -14,12 +11,6 @@ import ups.edu.ec.bibleoteca.interfazclases.Creacion.InterfazCreacion;
 public class InterfazBibleoteca {
 
     public static void main(String[] args) {
-         Frame frame = new Frame("Ventana");
-         frame.setSize(900, 900);
-         InterfazCreacion interfazCreacion= new InterfazCreacion();
-         frame.add(interfazCreacion.getPanelInterfaz());
-         
-         frame.setVisible(true);
-         
+        System.out.println("Hello World!");
     }
 }
